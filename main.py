daa7c7e31fdf040f1cc282baaba32f41033406ca
Archiet <archiettanwar71@gmail.com>
2025-08-06 00:47:05 +0530

    creating pdfs

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from fpdf import FPDF
 import pandas as pd
+import glob
+from pathlib import Path
 
-inv1=pd.read_excel("invoices/10001-2023.1.18.xlsx")
-inv2=pd.read_excel("10002-2023.1.18.xlsx")
-inv3=pd.read_excel("10003-2023.1.18.xlsx")
+filepaths=glob.glob('invoices/*.xlsx')
+for i in filepaths:
+    df=pd.read_excel(i,sheet_name="Sheet 1")
+    print(df)
+for i in filepaths:
+    filename=Path(i).stem
+    files=filename.split("-")
+    df=pd.read_excel(i,sheet_name="Sheet 1")
+    pdf=FPDF(orientation="L",
+             unit="mm",
+             format="A4")
+    pdf.set_font(family="Times",style="B",size=25)
+    pdf.add_page()
+    pdf.cell(w=0,h=0,txt="Tax Invoice - "+files[0],border=0,align="L",ln=1)
+    pdf.cell(w=0,h=20,txt="Date - "+files[1],border=0,align="L")
 
+
+
+    pdf.output(f"pdfs/{files[0]}.pdf")
