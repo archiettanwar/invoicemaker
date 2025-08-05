2d3e5377826c9a4dff6fb567156372039c659633
Archiet <archiettanwar71@gmail.com>
2025-08-06 00:10:29 +0530

    start

---
+++ main.py
@@ -0,0 +1,7 @@
+from fpdf import FPDF
+import pandas as pd
+
+inv1=pd.read_excel("invoices/10001-2023.1.18.xlsx")
+inv2=pd.read_excel("10002-2023.1.18.xlsx")
+inv3=pd.read_excel("10003-2023.1.18.xlsx")
+
